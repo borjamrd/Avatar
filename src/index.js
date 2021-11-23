1e65a0cef3469e443a611e6b17c499511ceec180
fernandocieri <fernandocieri@gmail.com>
2021-11-23 22:49:47 +0100

    Added conditional render of editable fields

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import photo from './Linkedin.jpeg'
+/*import photo from './foto-borja.png'*/
 import "./styles.css";
-/* 
+/* Primera tarea borja
 let data = {image:photo,name: "Borja Muñoz", email: "borjamunoz@europapress.es", phone: "+34 722 11 21 11", devices: "Mobile", location: "Madrid, Spain", RRSS: "Facebook", currency: "$", totalamount: "2,309"};
-
 class Avatar extends React.Component{
   constructor(props){
     super(props)
@@ -32,47 +31,52 @@ class Avatar extends React.Component{
 }
 } */
 
-let data = { image: photo, name: "Borja Muñoz", email: "borjamunoz@europapress.es", user: "cchu01", language: "English (United States)", PrivacySettings: "Only administrators and other instructors can view my profile information" };
+let data = { image: 'hey', name: "Borja Muñoz", email: "borjamunoz@europapress.es", user: "cchu01", language: "English (United States)", PrivacySettings: "Only administrators and other instructors can view my profile information" };
 
 let basicInfo = {
   title: "Basic info",
   elements: [
-    { title: "Full Name", text: data.name, isEditable: true },
-    { title: "Email Address", text: data.email, isEditable: false },
-    { title: "Student ID", text: <a href="#">Add student ID</a> },
-    { title: "Password", text: <a href="#">Change Password</a> }
+    { title: "Full Name", text: data.name, isEditable: true, isText: true },
+    { title: "Email Address", text: data.email, isEditable: true, isText: true },
+    { title: "Student ID", text: (<a href="#">Add student ID</a>), isEditable: false, isText: false },
+    { title: "Password", text: <a href="#">Change Password</a>, isEditable: false, isText: true }
   ]
 }
 
 let additionalInfo = {
   title: "Additional info",
   elements: [
-    { title: "Gender", text: <a href="#">Add Gender</a>, isEditable: false }
+    { title: "Gender", text: <a href="#">Add Gender</a>, isEditable: false, isText: true }
   ]
 }
 
 let systemSettings = {
   title: 'System settings',
   elements: [
-    { title: "Language", text: data.language, isEditable: true },
-    { title: "Privacy Settings", text: data.PrivacySettings, isEditable: true },
-    { title: "Global Notification Settings", text: <div><input type="checkbox"/>Stream Notifications<input type="checkbox"/>Email Notifications<input type="checkbox"/>Push Notifications</div>}
+    { title: "Language", text: data.language, isEditable: true, isText: true },
+    { title: "Privacy Settings", text: data.PrivacySettings, isEditable: true, isText: true },
+    { title: "Global Notification Settings", text: <div><input type="checkbox" />Stream Notifications<input type="checkbox" />Email Notifications<input type="checkbox" />Push Notifications</div> }
   ]
 }
-
 
 class Profile extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {...props.data} };
-  
+    this.state = { ...props.data }
+  };
+
   render() {
     return (
-      <>
-        <ProfileHeader data={data}/>
-        <UserInformation data={basicInfo}/>
-        <UserInformation data={additionalInfo}/>
-        <UserInformation data={systemSettings}/>
+      <><div className="topSpace"></div>
+        <ProfileHeader data={data} />
+        <div className="section1">
+          <UserInformation data={basicInfo} />
+          <UserInformation data={additionalInfo} />
+        </div>
+        <div className="section2">
+          <UserInformation data={systemSettings} />
+        </div>
+
       </>
     )
   }
@@ -81,11 +85,11 @@ class Profile extends React.Component {
 class ProfileHeader extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {...props.data};
+    this.state = { ...props.data };
   }
   render() {
     return (
-      <div className="profile-info">        
+      <div className="profile-info">
         <img src={this.state.image} className="avatar-img" alt="#"></img>
         <div>{this.state.name}</div>
         <div>{this.state.user}</div>
@@ -94,29 +98,70 @@ class ProfileHeader extends React.Component {
   }
 }
 
+
 class UserInformation extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {...props.data}
+    this.state = { ...props.data }
   }
-  render(){
-    let informationItem = this.state.elements.map((element)=>
-    <>
-    <div className="informationItem">
-     <div>{element.title}</div>
-     <div>{element.text}</div>
-    </div>
-    </>);
-    return(
+
+  render() {
+    let informationItem = this.state.elements.map((element) =>
+      <>
+        <Item element={element} />
+      </>);
+    return (
       <div>
         <h3>{this.state.title}</h3>
         {informationItem}
       </div>
-      
     )
   }
-  
 }
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...this.props.element }
+    this.editField = this.editField.bind(this);
+  }
+
+  editField() {
+    this.setState(prevState => (
+      { isText: !prevState.isText }));
+  }
+  /*function showButton(props){
+    constructor(props) {
+      super(props);
+      this.state = { ...this.props.element }
+      this.editField = this.editField.bind(this);
+    )
+  }*/
+
+
+
+  render() {
+    let myRender = undefined
+    {
+      if (this.state.isEditable) {
+        if (this.state.isText) {
+          myRender = <div> {this.state.text} <button onClick={this.editField} >Editar</button></div>
+        } else {
+          myRender = <div> <input type="text"></input> <button onClick={this.editField} >No Editar</button></div>
+        }
+      } else {
+        myRender = <div> {this.state.text} </div>
+      }
+    }
+
+    return (
+      <div className="informationItem">
+        {myRender};
+      </div>
+    )
+  }
+}
+// 
 
 ReactDOM.render(<Profile />, document.getElementById('root'));
 
